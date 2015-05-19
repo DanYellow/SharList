93079249b78d8a6f40b6dfc8ff3ac4d34c6bc3e8
Dan42K <jeanlouis.danielo@sfr.fr>
2015-05-19 23:36:13 +0200

    add PostCommentViewController

---
+++ SharList/PostCommentViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostCommentViewController.h
+//  SharList
+//
+//  Created by Jean-Louis Danielo on 19/05/2015.
+//  Copyright (c) 2015 Jean-Louis Danielo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostCommentViewController : UIViewController
+
+@end
---
+++ SharList/PostCommentViewController.m
@@ -0,0 +1,37 @@
+//
+//  PostCommentViewController.m
+//  SharList
+//
+//  Created by Jean-Louis Danielo on 19/05/2015.
+//  Copyright (c) 2015 Jean-Louis Danielo. All rights reserved.
+//
+
+#import "PostCommentViewController.h"
+
+@interface PostCommentViewController ()
+
+@end
+
+@implementation PostCommentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
