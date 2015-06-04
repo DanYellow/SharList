442ad4bc5a5258b25cf11005bb57ae579f5c2e43
Dan42K <jeanlouis.danielo@sfr.fr>
2015-06-05 00:50:17 +0200

    add new pods

---
+++ Pods/Target Support Files/Pods-SharList-FBSDKCoreKit/Pods-SharList-FBSDKCoreKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SharList_FBSDKCoreKit : NSObject
+@end
+@implementation PodsDummy_Pods_SharList_FBSDKCoreKit
+@end
---
+++ Pods/Target Support Files/Pods-SharList-FBSDKLoginKit/Pods-SharList-FBSDKLoginKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SharList_FBSDKLoginKit : NSObject
+@end
+@implementation PodsDummy_Pods_SharList_FBSDKLoginKit
+@end
