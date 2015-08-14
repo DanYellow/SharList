4887fda7f2cc82550c508e98ccfe04dfea88c540
Dan42K <jeanlouis.danielo@sfr.fr>
2015-08-14 21:16:28 +0200

    add SHDUserDiscovered class

---
+++ SharList/SHDUserDiscovered.h
@@ -0,0 +1,13 @@
+//
+//  SHDUserDiscovered.h
+//  SharList
+//
+//  Created by Jean-Louis Danielo on 14/08/2015.
+//  Copyright (c) 2015 Jean-Louis Danielo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHDUserDiscovered : UIView
+
+@end
---
+++ SharList/SHDUserDiscovered.m
@@ -0,0 +1,21 @@
+//
+//  SHDUserDiscovered.m
+//  SharList
+//
+//  Created by Jean-Louis Danielo on 14/08/2015.
+//  Copyright (c) 2015 Jean-Louis Danielo. All rights reserved.
+//
+
+#import "SHDUserDiscovered.h"
+
+@implementation SHDUserDiscovered
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
